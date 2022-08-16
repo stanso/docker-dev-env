e096aead988b1bf76a5a9af7bd72e867572105ec
Tianshuo Su <tssu32@gmail.com>
2022-08-16 13:27:37 -0500

    add docker-compose for ssh deamon docker dev env.

--- ubuntu_dev_env.sh
+++ ubuntu_dev_env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 docker run --device /dev/fuse \
            --cap-add SYS_ADMIN \
-           -v ~/Projects/docker-dev-data/home:/home/tssu \
-           -v ~/Projects/docker-dev-data/opt:/opt \
+           -v ~/Projects/docker-dev-env/ubuntu-data/home:/home/tssu \
+           -v ~/Projects/docker-dev-env/ubuntu-data/opt:/opt \
            -e TERM=$TERM \
            --rm -it \
            stanso/ubuntu-dev-env
